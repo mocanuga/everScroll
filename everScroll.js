67c4b98f1f777f99dfcde837eda5260147531753
Adi <mocanugeorge@gmail.com>
2015-11-04 14:43:18 +0200

    Update everScroll.js

--- everScroll.js
+++ everScroll.js
@@ -16,33 +16,42 @@
 		var opts = extend({
 			upScroll: null,
 			downScroll: null,
+			autoAttach: true,
 			threshold: 100 // px to the edge (top or bottom)
 		}, options),
-		lastScroll = 0;
+		lastScroll = 0,
+		attached = false;
+		if(opts.autoAttach)
+			attach();
 		function handler(e) {
 			var direction = window.scrollY > lastScroll ? 'down' : 'up',
-				overThreshold = opts.threshold > document.body.clientHeight - window.innerHeight - window.scrollY;
+				overThreshold = opts.threshold > document.body.clientHeight - window.innerHeight - window.scrollY || opts.threshold < window.scrollY;
+			e.lastScroll = lastScroll;
+			e.currentScroll = window.scrollY;
 			if(typeof callback === 'function' && overThreshold)
 				callback.apply(this, [direction]);
 			if(direction === 'down' && overThreshold) {
-				opts.downScroll.call(this);
+				opts.downScroll.apply(this, [e]);
 			}
-			if(direction === 'up' && overThreshold)
-				opts.upScroll.call(this);
+			if(direction === 'up' && overThreshold) {
+				opts.upScroll.apply(this, [e]);
+			}
 			lastScroll = window.scrollY;
 		}
-		return {
-			attached: false,
-			attach: function () {
-				if(this.attached)
-					window.removeEventListener('scroll', handler);
-				window.addEventListener('scroll', handler);
-				this.attached = true;
-			}, 
-			dettach: function () {
+		function attach() {
+			if(attached)
 				window.removeEventListener('scroll', handler);
-				this.attached = false; 
-			} 
+			window.addEventListener('scroll', handler, false);
+			attached = true;
+		}
+		function dettach() {
+			window.removeEventListener('scroll', handler);
+			attached = false; 
+		}
+		return {
+			attached: attached,
+			attach: attach, 
+			dettach: dettach
 		}; 
 	}; 
 	window.Scroll = everScroll; 
